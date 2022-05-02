a5888ec6403a549c33aabe75808ca8be0192c285
Luceta <57941164+Luceta@users.noreply.github.com>
2022-05-02 23:01:03 +0900

    ADD: Tv

--- Screens/Tv.tsx
+++ Screens/Tv.tsx
@@ -1,9 +1,49 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { useQuery, useQueryClient } from "react-query";
+import { tvApi } from "../api";
+import { ScrollView, RefreshControl } from "react-native";
+import Loader from "./Loader";
+import HList, { HListSeparator } from "../components/HList";
 
-const Tv = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Tv</Text>
-  </View>
-);
+const Tv = () => {
+  const queryClient = useQueryClient();
+  const {
+    isLoading: todayLoading,
+    data: todayData,
+    isRefetching: todayRefetching,
+  } = useQuery(["tv", "today"], tvApi.airingToday);
+  const {
+    isLoading: topLoading,
+    data: topData,
+    isRefetching: topRefetching,
+  } = useQuery(["tv", "top"], tvApi.topRated);
+  const {
+    isLoading: trendingLoading,
+    data: trendingData,
+    isRefetching: trendingRefetching,
+  } = useQuery(["tv", "trending"], tvApi.trending);
+
+  const onRefresh = () => {
+    queryClient.refetchQueries(["tv"]);
+  };
+  const refreshing = todayRefetching || topRefetching || trendingRefetching;
+
+  const loading = todayLoading || topLoading || trendingLoading;
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+      contentContainerStyle={{ paddingVertical: 30 }}
+    >
+      <HList title="Trending Tv" data={trendingData.results}></HList>
+      <HList title="Today Tv" data={todayData.results}></HList>
+      <HList title="Top-rated Tv" data={topData.results}></HList>
+    </ScrollView>
+  );
+};
 export default Tv;
